f2d6b454ceec41023de269e58dd96dee51019c6c
vishalcroma <59353994+vishalcroma@users.noreply.github.com>
2021-10-07 15:15:48 +0530

    Update Hello.java
    update

--- Hello.java
+++ Hello.java
@@ -2,8 +2,10 @@ public class{
 
 public static void main(String[] args)
 {
+  for(int i=0;i<=10;i++){
 
-System.out.println("Hello Java World");
+System.out.println("Hello Java World"+i);
+  }
 
 }
 
